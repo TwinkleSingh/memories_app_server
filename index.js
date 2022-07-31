4ff7a12337d1574e5f119fbb10ae2a722b898ee5
twinkle <sameeera97@gmail.com>
2022-07-31 18:06:58 +0530

    make it better

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 });
 
 //https://www.mongodb.com/cloud/atlas 
-const PORT = process.env.PORT; 
+const PORT = process.env.PORT || 5000; 
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser : true, useUnifiedTopology : true })
 .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
